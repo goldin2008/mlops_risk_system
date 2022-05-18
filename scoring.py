7fb7699239d0cba3ac4e28f27574929b36c9b844
goldin2008 <goldin20082011@gmail.com>
2022-05-18 15:01:47 -0400

    Update scoring.

--- scoring.py
+++ scoring.py
@@ -6,6 +6,7 @@ import os
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 import json
 
 
@@ -14,7 +15,7 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+output_model_path = os.path.join(config['output_model_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
 
 
@@ -23,3 +24,22 @@ def score_model():
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
 
+    # Take trained model
+    model_path = os.path.join(output_model_path, "trainedmodel.pkl")
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+
+    test_df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+
+    y_test = test_df.pop('exited')
+    X_test = test_df.drop(['corporation'], axis=1)
+
+    y_pred = model.predict(X_test)
+    score = metrics.f1_score(y_test, y_pred)
+
+    score_path = os.path.join(output_model_path, "latestscore.txt")
+    with open(score_path, 'w') as file_write:
+        file_write.write(str(score))
+
+if __name__ == "__main__":
+    score_model()
