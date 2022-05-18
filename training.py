821e1f5e7209a158774b00ed1e485e5813b4a581
goldin2008 <goldin20082011@gmail.com>
2022-05-18 14:46:54 -0400

    Update training.

--- training.py
+++ training.py
@@ -20,13 +20,30 @@ model_path = os.path.join(config['output_model_path'])
 def train_model():
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+                    multi_class='auto', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    #fit the logistic regression to your data  
+    df = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
+    # encode corporation code to numeric value
+    df['corporation'] = df['corporation'].apply(lambda x: sum(bytearray(x, 'utf-8')))
+    df = df.drop("corporation", axis=1)
+    y = df["exited"]
+    X = df.drop("exited", axis=1)
+    model.fit(X, y)
 
+    #write the trained model to your workspace in a file called trainedmodel.pkl
+    if not os.path.exists(config["output_model_path"]):
+        os.makedirs(config["output_model_path"])
+
+    model_output_path = os.path.join(
+        config["output_model_path"], "trainedmodel.pkl"
+    )
+
+    pickle.dump(model, open(model_output_path, "wb"))
+
+if __name__ == "__main__":
+    train_model()
