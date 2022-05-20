f8bc8d22a384ed7a3db594ce4cc94f5e6fe04ba2
goldin2008 <goldin20082011@gmail.com>
2022-05-19 22:32:49 -0400

    Update API.

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,37 @@
+import os
 import requests
+import json
+import subprocess
 
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+# URL = "http://127.0.0.1/"
+URL = "http://127.0.0.1:8000/"
 
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
 
+output_model_path = os.path.join(config['output_model_path'])
 
 #Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+#### Request module solution####
+response1 = requests.get(f'{URL}/prediction?filename=testdata.csv').content #put an API call here
+response2 = requests.get(f'{URL}/scoring').content #put an API call here
+response3 = requests.get(f'{URL}/summarystats?filename=testdata.csv').content #put an API call here
+response4 = requests.get(f'{URL}/diagnostics?filename=testdata.csv').content #put an API call here
+
+#### Command-line solution####
+response5=subprocess.run(['curl', '127.0.0.1:8000/prediction?filename=testdata.csv'],capture_output=True).stdout
+response6=subprocess.run(['curl', '127.0.0.1:8000/scoring'],capture_output=True).stdout
+response7=subprocess.run(['curl', '127.0.0.1:8000/summarystats?filename=testdata.csv'],capture_output=True).stdout
+response8=subprocess.run(['curl', '127.0.0.1:8000/diagnostics?filename=testdata.csv'],capture_output=True).stdout
 
 #combine all API responses
-responses = #combine reponses here
+responses = (
+    f"Perdiction:\n {response1}\n"
+    f"Scoring:\n  {response2}\n"
+    f"Summary Stats:\n {response3}\n"
+    f"Diagnostics:\n {response4}") #combine reponses here
 
 #write the responses to your workspace
-
-
-
+with open(os.path.join(output_model_path, "apireturns.txt"), 'w') as output_file:
+    output_file.writelines(responses)
--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, session, jsonify, request
+"""
+Flask API with:
+- Prediction Endpoint
+- Scoring Endpoint
+- Summary Statistic Endpoint
+- Diagnostics Endpoint
+"""
+from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
-
+import diagnostics
+import scoring
+import subprocess
 
 
 ######################Set up variables for use in our script
@@ -18,33 +24,52 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-
-prediction_model = None
+test_data_path = os.path.join(config['test_data_path']) 
+output_model_path = os.path.join(config['output_model_path'])
 
 
 #######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
+@app.route("/prediction", methods=['GET','OPTIONS'])
 def predict():        
     #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+    filename = request.args.get('filename')
+    file_path = os.path.join(test_data_path, filename)
+    _, X, _ = diagnostics.load_data(file_path)
+    y_pred = diagnostics.model_predictions(X)
+    return str(y_pred) #add return value for prediction outputs
 
 #######################Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
+def score():        
     #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    # score = subprocess.run(['python', 'scoring.py'], capture_output=True).stdout
+    score = scoring.score_model()
+    return str(score) #add return value (a single F1 score number)
 
 #######################Summary Statistics Endpoint
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def stats():        
     #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    filename = request.args.get('filename')
+    file_path = os.path.join(test_data_path, filename)
+    df_data, _, _ = diagnostics.load_data(file_path)
+    data_sum = diagnostics.dataframe_summary(df_data)
+    return str(data_sum) #return a list of all calculated summary statistics
 
 #######################Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
+def diag():        
     #check timing and percent NA values
-    return #add return value for all diagnostics
+    filename = request.args.get('filename')
+    file_path = os.path.join(test_data_path, filename)
+    df_data, _, _ = diagnostics.load_data(file_path)
+
+    missing = diagnostics.missing_data(df_data)
+    exe_time = diagnostics.execution_time()
+    outdated = diagnostics.outdated_packages_list()
+
+    diag_res = "Missing Data: \n{} \n\nExe Time:\nIngestion:{} \nTraining:{} \n\nOutdated Packages:\n{}".format(missing, exe_time[0], exe_time[1], outdated)
+    return diag_res #add return value for all diagnostics
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
